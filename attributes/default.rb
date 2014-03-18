dfa985e37e76003f06d096f4430ceaac1959bf5e
Homer Strong <homer.strong@gmail.com>
2014-03-18 10:59:19 -0700

    work around (broken?) invalid default for replica.fetch.max.bytes

--- attributes/default.rb
+++ attributes/default.rb
@@ -40,6 +40,7 @@ default[:kafka][:consumer_groupid] = "test-consumer-group"
 default[:kafka][:producer_type] = "sync"
 default[:kafka][:producer_broker_list] = "localhost:9092"
 default[:kafka][:mirrormaker_whitelist] = "\".*\""
+default[:kafka][:replica_fetch_max_bytes] = 104857600
 
 default[:kafka][:user] = "kafka"
 default[:kafka][:group] = "kafka"
